a2038715e51778111290ad75edb88abc240cc7bc
Kat <10361870+katattakd@users.noreply.github.com>
2021-02-09 02:23:50 -0800

    Added TLS support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![warn(clippy::all)]
 
 use actix_web::{get, web, guard, body::{Body, ResponseBody}, dev::ServiceResponse, http::{header, header::{ContentType, IntoHeaderValue}, Method, StatusCode}, middleware::{Compress, Logger, DefaultHeaders, NormalizePath, TrailingSlash, ErrorHandlers, ErrorHandlerResponse}, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use std::{process, fs, collections::BTreeMap, net::SocketAddr, ffi::OsStr, path::{Path, PathBuf}};
+use std::{process, fs, fs::File, collections::BTreeMap, net::SocketAddr, ffi::OsStr, path::{Path, PathBuf}, io::BufReader, sync::Arc};
 use serde_derive::Deserialize;
 use log::{trace, warn, debug, error, log_enabled, info, Level, LevelFilter};
+use rustls::{NoClientAuth, ServerConfig, ResolvesServerCertUsingSNI, sign, sign::CertifiedKey, internal::pemfile};
 
 #[derive(Deserialize,Clone,Debug)]
 struct Config {
@@ -17,6 +18,7 @@ struct Vhost {
 	host: String,
 	files: Vec<Files>,
 	redir: Vec<Redir>,
+	tls: Option<Tls>,
 }
 
 #[derive(Deserialize,Clone,Debug)]
@@ -33,8 +35,16 @@ struct Redir {
 }
 
 #[derive(Deserialize,Clone,Debug)]
+struct Tls {
+	cert_file: PathBuf,
+	key_file: PathBuf,
+	//hsts: bool,
+}
+
+#[derive(Deserialize,Clone,Debug)]
 struct Server {
 	http_bind: Vec<SocketAddr>,
+	tls_bind: Vec<SocketAddr>,
 	log_format: String,
 }
 
@@ -42,7 +52,8 @@ struct Server {
 // - finish implementing web server
 //   - implement form handling
 //   - implement http auth
-//   - implement https + hsts
+//   - implement hsts
+//   - clean up code
 //   - implement http reverse proxy
 // - implement page generation
 //   - katsite code may be useful as a reference
@@ -70,13 +81,55 @@ async fn main() {
 	trace!("reading config.toml");
 	let config_data = fs::read_to_string("config.toml").unwrap_or_else(|err| {
 		error!("Unable to read config file! {}", err);
-		process::exit(exitcode::IOERR);
+		process::exit(exitcode::NOINPUT);
 	});
 	trace!("parsing config.toml as toml");
 	let config: Config = toml::from_str(&config_data).unwrap_or_else(|err| {
 		error!("Unable to parse config file! {}", err);
 		process::exit(exitcode::CONFIG);
 	});
+
+	trace!("configuring rustls");
+	let mut tlsconf = ServerConfig::new(NoClientAuth::new());
+	let mut resolver = ResolvesServerCertUsingSNI::new();
+	for vhost in &config.vhost {
+		if let Some(tls) = &vhost.tls {
+			let cert_file = &mut BufReader::new(File::open(&tls.cert_file).unwrap_or_else(|err| {
+				error!("Unable to open TLS cert for {}! {}", vhost.host, err);
+				process::exit(exitcode::NOINPUT);
+			}));
+			let key_file = &mut BufReader::new(File::open(&tls.key_file).unwrap_or_else(|err| {
+				error!("Unable to open TLS private key for {}! {}", vhost.host, err);
+				process::exit(exitcode::NOINPUT);
+			}));
+
+			let certs = pemfile::certs(cert_file).unwrap_or_else(|_| {
+				error!("Unable to load TLS cert for {}!", vhost.host);
+				process::exit(exitcode::DATAERR);
+			});
+			let keys = pemfile::pkcs8_private_keys(key_file).unwrap_or_else(|_| {
+				error!("Unable to load TLS private key for {}!", vhost.host);
+				process::exit(exitcode::DATAERR);
+			});
+			if keys.len() > 1 {
+				debug!("more than one TLS key provided for {}", vhost.host)
+			}
+			let key = keys.get(0).unwrap_or_else(|| {
+				error!("No TLS private keys found for {}!", vhost.host);
+				process::exit(exitcode::DATAERR);
+			});
+			let signingkey = sign::any_supported_type(key).unwrap_or_else(|_| {
+				error!("Unable to parse TLS private key for {}!", vhost.host);
+				process::exit(exitcode::DATAERR);
+			});
+
+			resolver.add(&vhost.host, CertifiedKey::new(certs, Arc::new(signingkey))).unwrap_or_else(|err| {
+				error!("Unable to configure TLS cert for {}! {}", vhost.host, err);
+				process::exit(exitcode::DATAERR);
+			});
+		}
+	}
+	tlsconf.cert_resolver = Arc::new(resolver);
 
 	trace!("configuring HttpServer");
 	let conf = config.to_owned();
@@ -137,6 +190,12 @@ async fn main() {
 	trace!("adding port bindings");
 	for addr in config.server.http_bind {
 		server = server.bind(addr).unwrap_or_else(|err| {
+			error!("Unable to bind to port! {}", err);
+			process::exit(exitcode::OSERR);
+		})
+	}
+	for addr in config.server.tls_bind {
+		server = server.bind_rustls(addr, tlsconf.to_owned()).unwrap_or_else(|err| {
 			error!("Unable to bind to port! {}", err);
 			process::exit(exitcode::OSERR);
 		})
