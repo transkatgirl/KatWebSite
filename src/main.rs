55b4734c66feabf223a0ddc5ea0e22a220826182
Kat <10361870+katattakd@users.noreply.github.com>
2021-02-12 23:22:07 -0800

    Improved vhost & redirect configuration

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![warn(clippy::all)]
 
-use actix_web::{get, web, guard, body::{Body, ResponseBody}, dev::ServiceResponse, http::{header, header::{ContentType, IntoHeaderValue}, Method, StatusCode, Uri, uri::Scheme}, middleware::{Compress, Logger, DefaultHeaders, NormalizePath, TrailingSlash, ErrorHandlers, ErrorHandlerResponse}, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, guard, body::{Body, ResponseBody}, dev::ServiceResponse, http::{header, header::{ContentType, IntoHeaderValue}, Method, StatusCode, Uri, uri::Scheme}, middleware::{Compress, Logger, DefaultHeaders, NormalizePath, TrailingSlash, ErrorHandlers, ErrorHandlerResponse}, App, Scope, HttpRequest, HttpResponse, HttpServer, Responder};
 use std::{process, iter, fs, fs::File, collections::BTreeMap, net::SocketAddr, ffi::OsStr, path::{Path, PathBuf}, io::BufReader, sync::Arc, error::Error, boxed::Box};
 use serde_derive::Deserialize;
 use log::{trace, warn, debug, error, info, LevelFilter};
@@ -17,8 +17,9 @@ struct Config {
 #[derive(Deserialize,Clone,Debug)]
 struct Vhost {
 	host: String,
-	files: Vec<Files>,
-	redir: Vec<Redir>,
+	protocols: Vec<String>,
+	files: Option<Vec<Files>>,
+	redir: Option<Vec<Redir>>,
 	tls: Option<Tls>,
 }
 
@@ -52,7 +53,7 @@ struct Server {
 // - finish implementing web server
 //   - implement form handling
 //   - implement http auth
-//   - implement http -> https redirects
+//   - implement per-vhost headers
 //   - clean up code
 //   - implement http reverse proxy
 // - implement page generation
@@ -64,22 +65,16 @@ fn handle_not_found() -> HttpResponse {
 		.body("<!DOCTYPE html><h3 style=\"font: 20px sans-serif; margin: 12px\">The requested resource could not be found.</h3>")
 }
 
-fn handle_redirect(status: web::Data<StatusCode>, dest: web::Data<String>) -> HttpResponse {
+fn handle_redirect(req: HttpRequest, status: web::Data<StatusCode>, dest: web::Data<String>) -> HttpResponse {
+	let mut dest = dest.to_string();
+	for (_, segment) in req.match_info().iter() {
+		dest = [&dest,"/",segment].concat()
+	}
+
 	HttpResponse::build(*status.as_ref())
-		.header(header::LOCATION, dest.as_str())
+		.header(header::LOCATION, dest)
 		.finish()
 }
-
-// FIXME
-/*fn handle_redirect_to_https(req: HttpRequest) -> HttpResponse {
-	let mut parts = req.uri().to_owned().into_parts();
-	parts.scheme = Some(Scheme::HTTPS);
-	let uri = Uri::from_parts(parts).unwrap();
-
-	HttpResponse::TemporaryRedirect()
-		.header(header::LOCATION, uri.to_string())
-		.finish()
-}*/
 
 fn create_certified_key(pemfiles: &[PathBuf]) -> Result<CertifiedKey, Box<dyn Error>> {
 	let mut certs = Vec::new();
@@ -99,6 +94,51 @@ fn create_certified_key(pemfiles: &[PathBuf]) -> Result<CertifiedKey, Box<dyn Er
 	let signingkey = sign::any_supported_type(key).or(Err("unable to parse key"))?;
 
 	Ok(CertifiedKey::new(certs, Arc::new(signingkey)))
+}
+
+fn configure_vhost_scope(vhost: &Vhost, is_tls: bool) -> Option<Scope> {
+	let is_active = match is_tls {
+		true => vhost.protocols.contains(&"https".to_string()),
+		false => vhost.protocols.contains(&"http".to_string()),
+	};
+
+	if !is_active {
+		return None
+	}
+
+	let mut scope = web::scope("/")
+		.guard(guard::Host(String::from(&vhost.host)));
+
+	for redir in vhost.redir.to_owned().unwrap_or_else(|| Vec::new()) {
+		let status = match redir.permanent {
+			true => StatusCode::PERMANENT_REDIRECT,
+			false => StatusCode::TEMPORARY_REDIRECT,
+		};
+		let target = match redir.target.as_ref() {
+			"/" => "",
+			_ => &redir.target,
+		};
+		scope = scope.service(
+			web::resource(target)
+				.data(status).data(redir.dest)
+				.to(handle_redirect)
+		)
+	}
+
+	for files in vhost.files.to_owned().unwrap_or_else(|| Vec::new()) {
+		let mount = match files.mount.as_ref() {
+			"/" => "",
+			_ => &files.mount,
+		};
+		scope = scope.service(
+			actix_files::Files::new(mount, &files.file_dir)
+				.index_file("index.html")
+				.prefer_utf8(true)
+				.disable_content_disposition()
+		)
+	}
+	
+	Some(scope)
 }
 
 #[actix_web::main]
@@ -139,9 +179,7 @@ async fn main() {
 
 	trace!("configuring HttpServer");
 	let conf = config.to_owned();
-	let appbuilder = move || {
-		trace!("generating application builder");
-
+	let appbuilder = move |is_tls| {
 		let mut headers = DefaultHeaders::new();
 		for (key, val) in &conf.headers {
 			headers = headers.header(key, val);
@@ -154,46 +192,25 @@ async fn main() {
 			.default_service(web::route().to(handle_not_found));
 
 		for vhost in &conf.vhost {
-			let mut scope = web::scope("/").guard(guard::Host(
-				String::from(&vhost.host)
-			));
-
-			for redir in vhost.redir.to_owned() {
-				let status = match redir.permanent {
-					true => StatusCode::PERMANENT_REDIRECT,
-					false => StatusCode::TEMPORARY_REDIRECT,
-				};
-				scope = scope.service(web::resource(redir.target)
-					.data(status).data(redir.dest)
-					.to(handle_redirect)
-				)
-			}
-
-			for files in &vhost.files {
-				let mount = match files.mount.as_ref() {
-					"/" => "",
-					_ => &files.mount,
-				};
-				scope = scope.service(
-					actix_files::Files::new(mount, &files.file_dir)
-						.index_file("index.html")
-						.prefer_utf8(true)
-						.disable_content_disposition()
-				)
-			}
-
-			app = app.service(scope);
+			app = match configure_vhost_scope(vhost, is_tls) {
+				Some(scope) => app.service(scope),
+				None => app,
+			};
 		}
 
-		app/*.service(
-			web::scope("/")
-				.guard(guard::Post())
-				.service(handle_post)
-
-		)*/
+		app
 	};
+	let appbuilderr = appbuilder.to_owned();
 
-	let mut server = HttpServer::new(appbuilder);
+	let mut server = HttpServer::new(move || {
+		trace!("generating http application builder");
+		appbuilder(false)
+	});
+
+	let mut servertls = HttpServer::new(move || {
+		trace!("generating https application builder");
+		appbuilderr(true)
+	});
 
 	trace!("adding port bindings");
 	for addr in config.server.http_bind {
@@ -203,7 +220,7 @@ async fn main() {
 		})
 	}
 	for addr in config.server.tls_bind {
-		server = server.bind_rustls(addr, tlsconf.to_owned()).unwrap_or_else(|err| {
+		servertls = servertls.bind_rustls(addr, tlsconf.to_owned()).unwrap_or_else(|err| {
 			error!("Unable to bind to port! {}", err);
 			process::exit(exitcode::OSERR);
 		})
@@ -212,6 +229,7 @@ async fn main() {
 	info!("Loaded configuration.");
 
 	trace!("starting HttpServer");
+	servertls.run();
 	server.run().await.unwrap_or_else(|err| {
 			error!("Unable to start server! {}", err);
 			process::exit(exitcode::OSERR);
