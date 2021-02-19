9b63da81ecfe2950e062bc2ddfd25fb940f134d6
Kat <10361870+katattakd@users.noreply.github.com>
2021-02-19 03:03:12 -0800

    Added CLI flags, improved config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
 #![warn(clippy::all)]
 
 use actix_web::{web, guard, http::{header, StatusCode}, middleware::{Compress, Logger, DefaultHeaders, NormalizePath, TrailingSlash}, App, Scope, HttpRequest, HttpResponse, HttpServer};
-use std::{process, iter, fs, fs::File, collections::BTreeMap, net::SocketAddr, ffi::OsStr, path::{Path, PathBuf}, io::BufReader, sync::Arc, error::Error, boxed::Box};
-use serde_derive::Deserialize;
-use log::{trace, warn, debug, error, info, LevelFilter};
+use clap::Clap;
+use log::{trace, warn, debug, error, info};
 use rustls::{NoClientAuth, ServerConfig, ResolvesServerCertUsingSNI, sign, sign::CertifiedKey, PrivateKey, Certificate};
+use serde_derive::Deserialize;
+use std::{process, iter, fs, fs::File, collections::BTreeMap, net::SocketAddr, ffi::OsStr, path::{Path, PathBuf}, io::BufReader, sync::Arc, error::Error, boxed::Box};
+
+/// A minimal static site generator and web server.
+#[derive(Clap,Debug)]
+#[clap(version = "0.1.0")]
+struct Opts {
+	/// Specifies the configuration file to load.
+	#[clap(short, long, default_value = "config.toml")]
+	config: String,
+
+	/// Decreases log verbosity, ignored if RUST_LOG is set. Minimum possible log verbosity is -2.
+	#[clap(short, long, parse(from_occurrences))]
+	quiet: i32,
+
+	/// Increases log verbosity, ignored if RUST_LOG is set. Maximum possible log verbosity is 3.
+	#[clap(short, long, parse(from_occurrences))]
+	verbose: i32,
+}
+
 
 #[derive(Deserialize,Clone,Debug)]
 #[serde(deny_unknown_fields)]
 struct Config {
 	vhost: Vec<Vhost>,
+
+	#[serde(default)]
 	headers: BTreeMap<String, String>,
+
 	server: Server,
 }
 
@@ -19,24 +41,41 @@ struct Config {
 #[serde(deny_unknown_fields)]
 struct Vhost {
 	host: String,
+
+	#[serde(default = "default_vhost_protocols")]
 	protocols: Vec<String>,
-	files: Option<Vec<Files>>,
-	redir: Option<Vec<Redir>>,
+
+	#[serde(default)]
+	files: Vec<Files>,
+
+	#[serde(default)]
+	redir: Vec<Redir>,
+
 	tls: Option<Tls>,
+}
+
+fn default_vhost_protocols() -> Vec<String> {
+	vec!["http".to_string()]
 }
 
 #[derive(Deserialize,Clone,Debug)]
 #[serde(deny_unknown_fields)]
 struct Files {
+	#[serde(default)]
 	mount: String,
+
 	file_dir: PathBuf,
 }
 
 #[derive(Deserialize,Clone,Debug)]
 #[serde(deny_unknown_fields)]
 struct Redir {
+	#[serde(default)]
 	target: String,
+
 	dest: String,
+
+	#[serde(default)]
 	permanent: bool,
 }
 
@@ -50,9 +89,18 @@ struct Tls {
 #[derive(Deserialize,Clone,Debug)]
 #[serde(deny_unknown_fields)]
 struct Server {
+	#[serde(default)]
 	http_bind: Vec<SocketAddr>,
+
+	#[serde(default)]
 	tls_bind: Vec<SocketAddr>,
+
+	#[serde(default = "default_server_log_format")]
 	log_format: String,
+}
+
+fn default_server_log_format() -> String {
+	"%{Host}i %a \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %D".to_string()
 }
 
 // TODO:
@@ -109,7 +157,7 @@ fn configure_vhost_scope(vhost: &Vhost, is_tls: bool) -> Option<Scope> {
 	let mut scope = web::scope("/")
 		.guard(guard::Host(String::from(&vhost.host)));
 
-	for redir in vhost.redir.to_owned().unwrap_or_default() {
+	for redir in vhost.redir.to_owned() {
 		let status = match redir.permanent {
 			true => StatusCode::PERMANENT_REDIRECT,
 			false => StatusCode::TEMPORARY_REDIRECT,
@@ -128,7 +176,7 @@ fn configure_vhost_scope(vhost: &Vhost, is_tls: bool) -> Option<Scope> {
 	// Potentially useful future features:
 	// - https://github.com/actix/actix-web/issues/1718
 	// - https://github.com/actix/actix-web/issues/2000
-	for files in vhost.files.to_owned().unwrap_or_default() {
+	for files in vhost.files.to_owned() {
 		let mount = match files.mount.as_ref() {
 			"/" => "",
 			_ => &files.mount,
@@ -146,41 +194,36 @@ fn configure_vhost_scope(vhost: &Vhost, is_tls: bool) -> Option<Scope> {
 
 #[actix_web::main]
 async fn main() {
-	env_logger::Builder::new()
-		.filter_level(LevelFilter::Info)
-		.format_timestamp(Some(env_logger::fmt::TimestampPrecision::Millis))
-		.parse_default_env()
-		.init();
+	let opts: Opts = Opts::parse();
 
-	trace!("reading config.toml");
-	let config_data = fs::read_to_string("config.toml").unwrap_or_else(|err| {
+	let logstr = match opts.verbose - opts.quiet {
+		i32::MIN...-2 => "error",
+		-1 => "warn, actix_web::middleware::logger = info",
+		0 => "info, actix_server::accept = warn",
+		1 => "debug, actix_server::accept = warn",
+		2 => "trace, actix_web::middleware::logger = debug, rustls = debug, actix_server::accept = warn",
+		3...i32::MAX => "trace",
+	};
+
+	flexi_logger::Logger::with_env_or_str(logstr)
+		.start().unwrap();
+
+	trace!("started logger with default RUST_LOG set to {:?}", logstr);
+
+	info!("Loading configuration");
+
+	trace!("reading {}", opts.config);
+	let config_data = fs::read_to_string(&opts.config).unwrap_or_else(|err| {
 		error!("Unable to read config file! {}", err);
 		process::exit(exitcode::NOINPUT);
 	});
-	trace!("parsing config.toml as toml");
+	debug!("parsing {} as toml configuration", opts.config);
 	let config: Config = toml::from_str(&config_data).unwrap_or_else(|err| {
 		error!("Unable to parse config file! {}", err);
 		process::exit(exitcode::CONFIG);
 	});
 
-	trace!("loading tls certificates");
-	let mut resolver = ResolvesServerCertUsingSNI::new();
-	for vhost in &config.vhost {
-		if let Some(tls) = &vhost.tls {
-			let keypair = create_certified_key(&tls.pemfiles).unwrap_or_else(|err| {
-				error!("Unable to load certificate pair for {}! {}", vhost.host, err);
-				process::exit(exitcode::DATAERR);
-			});
-			resolver.add(&vhost.host, keypair).unwrap_or_else(|err| {
-				error!("Unable to configure certificate pair for {}! {}", vhost.host, err);
-				process::exit(exitcode::DATAERR);
-			});
-		}
-	}
-	let mut tlsconf = ServerConfig::new(NoClientAuth::new());
-	tlsconf.cert_resolver = Arc::new(resolver);
-
-	trace!("configuring HttpServer");
+	debug!("initializing HttpServer");
 	let conf = config.to_owned();
 	let appbuilder = move |is_tls| {
 		let mut headers = DefaultHeaders::new();
@@ -216,31 +259,46 @@ async fn main() {
 		appbuilderr(true)
 	});
 
-	trace!("adding port bindings");
 	for addr in config.server.http_bind {
 		server = server.bind(addr).unwrap_or_else(|err| {
 			error!("Unable to bind to port! {}", err);
 			process::exit(exitcode::OSERR);
 		})
 	}
-	for addr in &config.server.tls_bind {
-		servertls = servertls.bind_rustls(addr, tlsconf.to_owned()).unwrap_or_else(|err| {
-			error!("Unable to bind to port! {}", err);
-			process::exit(exitcode::OSERR);
-		})
-	}
+	let futureserver = server.run();
 
-	info!("Loaded configuration.");
-
-	trace!("starting HttpServer");
 	if !config.server.tls_bind.is_empty() {
+		debug!("loading tls certificates");
+		let mut resolver = ResolvesServerCertUsingSNI::new();
+		for vhost in &config.vhost {
+			if let Some(tls) = &vhost.tls {
+				let keypair = create_certified_key(&tls.pemfiles).unwrap_or_else(|err| {
+					error!("Unable to load certificate pair for {}! {}", vhost.host, err);
+					process::exit(exitcode::DATAERR);
+				});
+				resolver.add(&vhost.host, keypair).unwrap_or_else(|err| {
+					error!("Unable to configure certificate pair for {}! {}", vhost.host, err);
+					process::exit(exitcode::DATAERR);
+				});
+			}
+		}
+
+		let mut tlsconf = ServerConfig::new(NoClientAuth::new());
+		tlsconf.cert_resolver = Arc::new(resolver);
+
+		for addr in &config.server.tls_bind {
+			servertls = servertls.bind_rustls(addr, tlsconf.to_owned()).unwrap_or_else(|err| {
+				error!("Unable to bind to port! {}", err);
+				process::exit(exitcode::OSERR);
+			})
+		}
+
 		servertls.run();
-	} else {
-		debug!("tls server has no port bindings, skipping");
 	}
-	server.run().await.unwrap_or_else(|err| {
+
+	futureserver.await.unwrap_or_else(|err| {
 		error!("Unable to start server! {}", err);
 		process::exit(exitcode::OSERR);
 	});
-	trace!("HttpServer execution has stopped");
+	debug!("HttpServer execution has stopped");
 }
